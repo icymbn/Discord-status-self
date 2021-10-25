5ad700474e6948e295916909f7ecb811ccd4bca7
hadi <65118892+hadiazt@users.noreply.github.com>
2021-10-25 15:00:52 +0330

    better coding ;/

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 // ==============================
-const discord  = require('discord.js-selfbot');
+const discord  = require('discord.js-self'); // better that discord.js-selfbot
 const client = new discord.Client();
-const token = "" // <======= put your token here
-const owner = "" 
-const ownername = "ज्ञ  𝐌𝐚𝐧𝐢⸸𝐌𝐁𝐍 ᵍᵒʳᵍⁱ#0169" 
 
+const token = "" // <======= put your token here
+const owner = ""  // extra 
+const ownername = "ज्ञ  𝐌𝐚𝐧𝐢⸸𝐌𝐁𝐍 ᵍᵒʳᵍⁱ#0169" // extra 
+const status = [
+  "dnd", 
+  "idle", 
+  "online"
+]
+
+var finalstatus = status[Math.floor(Math.random()*status.length)];
+
+/* hadi.az was here ;D */
 // ===============================
 client.on('ready', () => {  
     console.log(`Logged in as ${client.user.tag}!`);
-	const stat = ["dnd", "dnd", "idle", "idle", "online", "online"];
   setInterval(() => {
-    const random = Math.floor(Math.random() * (stat.length - 1) + 1);
-	  const stats = stat[random];
-	console.log(`Status Changed`);
-    client.user.setPresence({
-        status: stats,
-        }
+		console.log(`Status Changed`);
+    	client.user.setPresence({
+        	status: finalstatus,
+        	}
       )
   }, 1000);
 
