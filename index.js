88b7a0cf14b27cb0ff548ac18aca0c464ff33e06
Mani-MBn <78789133+icymbn@users.noreply.github.com>
2021-10-25 14:09:14 +0330

    Create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+// ==============================
+const discord  = require('discord.js-selfbot');
+const client = new discord.Client();
+const token = "" //put your token here
+const owner = "" 
+const ownername = "ज्ञ  𝐌𝐚𝐧𝐢⸸𝐌𝐁𝐍 ᵍᵒʳᵍⁱ#0169" 
+
+// ===============================
+client.on('ready', () => {  
+    console.log(`Logged in as ${client.user.tag}!`);
+	const stat = ["dnd", "dnd", "idle", "idle", "online", "online"];
+  setInterval(() => {
+    const random = Math.floor(Math.random() * (stat.length - 1) + 1);
+	  const stats = stat[random];
+	console.log(`Status Changed`);
+    client.user.setPresence({
+        status: stats,
+        }
+      )
+  }, 1000);
+
+    });
+
+client.login(token);
